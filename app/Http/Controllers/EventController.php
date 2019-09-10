d091ee089406e0c69f1fce59adabd4cce85ccce7
ColePWarren <54299384+ColePWarren@users.noreply.github.com>
2019-09-10 10:59:35 -0700

    Deleting necessary methods to clean up code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,48 +26,7 @@ class EventController extends Controller
   {
     
   }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-
-  public function store(Request $request)
-  {
     
-  }
-   */
-    
-  
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-
-  public function show($id)
-  {
-    
-  }
-   */
-    
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-
-  public function edit($id)
-  {
-    
-  }
-   */
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
   public function update($id)
   {
     
