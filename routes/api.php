607fe5791d8aa3051ff705b821442110206e5393
ColePWarren <54299384+ColePWarren@users.noreply.github.com>
2019-09-10 10:34:58 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('users', 'UsersController');
+Route::resource('event', 'EventsController');
+Route::resource('plantInstance', 'PlantInstancesController');
+Route::resource('plantType', 'PlantTypesController');
