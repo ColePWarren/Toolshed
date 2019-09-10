bc9b0e84f643654fef347a650fa271dd1d339d27
ColePWarren <54299384+ColePWarren@users.noreply.github.com>
2019-09-10 16:33:41 -0700

    Update PlantInstance.php

--- app/PlantInstance.php
+++ app/PlantInstance.php
@@ -1,7 +1,17 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
-class PlantType extends Model
-{
 
+class PlantInstance extends Model
+{
+    public function validatePlantTypeOwner($plantInstanceId) {
+          $user = auth()->user();
+          $plantType = PlantType::findOrFail($plantInstanceId);
+
+          if ($plantType->userId == $user->id) {
+             return true;
+          } else {
+             return false;   
+          }
+      }   
 }
